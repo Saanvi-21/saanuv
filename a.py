61584afdba4d1951b87e073f648f9352cc004473
saanvi <71885805+Saanvi-21@users.noreply.github.com>
2021-02-16 17:38:13 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,40 @@
+#How to make your bed!
+print("How to make your bed")
+
+#Step 1
+print("Step 1")
+
+print("Clear the bed")
+
+#Step 2
+print("Step 2")
+
+print("Put the top sheet on")
+
+#Step 3
+print("Step 3")
+
+print("Make hospital corners")
+
+#Step 4
+print("Step 4")
+
+print("Place the blankets")
+
+#Step 5
+print("Step 5")
+
+print("Fold the top sheet and blanket on")
+
+#Step 6
+print("Step 6")
+
+print("Fluff the pillows and place them")
+
+#lastly
+print("Yay! Your bed is ready")
+
+
+
+
+
